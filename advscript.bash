6e9217dae434d868a069705b26171eecc1d1a90b
Jonn DevOps <jonndevops@gmail.com>
2025-03-10 09:25:24 +0000

    added more script lines

--- advscript.bash
+++ advscript.bash
@@ -6,6 +6,12 @@ read number
 if [ $number -eq 08166778179 ]
 then
 echo "your name is John"
+elif [ $number -eq 08173370513 ]
+then
+echo "your name is John"
+elif [ $number -eq 09049496530 ]
+then
+echo "your name is Favour Makanju"
 else
-echo "unidentified phone number"
+echo "unidentified phone number, please try again"
 fi
